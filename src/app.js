e5e360f9ddfb72d0ee2b5030580dd296acc3a49b
Rafael Molina Osias <engineer.raflash@gmail.com>
2024-03-06 07:16:16 -0500

    configuracion del servidor con socektio

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log("app");
+const express  = require('express');
+const path     = require('path');
+const sokectio = require('socket.io');
+const http     = require('http');
+const app      = express();
+
+app.set('port', 3001);
+app.use(express.static(path.join(__dirname,'public')));
+
+const server = http.createServer(app);
+const io     = sokectio(server);
+require('./sokect')(io);
+
+server.listen(app.get('port'),() => {
+    console.log('Aplicacion corriendo en el puerto '+ app.get('port'));
+});
--- src/sokect.js
+++ src/sokect.js
@@ -1,1 +1,4 @@
 console.log("sokect");
+module.exports = (io) => {
+    console.log('Archivo socket ha sido cargado');
+}
