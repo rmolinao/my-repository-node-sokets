c6f5cdec6f51a32884271d822e41a85ddf875251
Rafael Molina Osias <engineer.raflash@gmail.com>
2024-03-07 08:33:32 -0500

    emitiendo eventos socket tercera parte usando broadcast

--- src/public/index.js
+++ src/public/index.js
@@ -3,7 +3,9 @@ const socket = io();
 const username = document.getElementById('username');
 const white_message = document.getElementById('white_message');
 const all_messages = document.getElementById('all_messages');
-
+const new_user = document.getElementById('new_user');
+const writing =document.getElementById('writing');
+//eventos de la pagina
 white_message.addEventListener('keyup',evnt => {
     if (evnt.code == 'Enter') {
         if (username.value != '' && white_message.value != '') {
@@ -17,6 +19,27 @@ white_message.addEventListener('keyup',evnt => {
             console.log('Ingrese los campos Completos');
         }
     }
+});
+
+white_message.addEventListener('keydown',(event) => {
+    if (username.value !='') {
+        socket.emit('writing', username.value);
+    }
+});
+
+//eventos sockets
+socket.on('writing',(username) => {
+    writing.innerHTML = username + ' esta escribiendo';
+    setTimeout(() => {
+        writing.innerHTML ='';
+    },3000);
+})
+
+socket.on('newUser', (message) => {
+    new_user.innerHTML = message;
+    setTimeout(() => {
+        new_user.innerHTML ='';
+    },3000);
 });
 socket.on('messageslist', (messageslist) => {
     let content = '';
--- src/sokect.js
+++ src/sokect.js
@@ -2,8 +2,22 @@ let messageslist = [];
 module.exports = io_Client => {
     io_Client.on('connection', (socket) => {
         console.log('Un usuario se ha conectado');
+
+        /** Eventos que emite el servidor*/
+
+        //al conectarse un nuevo usuario se emite la lista de mensages del chat
         io_Client.emit('messageslist', messageslist);
-        //escucho el evento socket en emitido en el cliente
+
+        //el sokect del servidor emite un evento a los clientes
+        socket.broadcast.emit('newUser', 'Se ha conectado un nuevo usuario');
+
+
+        /** Eventos que escucha el servidor*/
+        socket.on('writing', (username) => {
+            socket.broadcast.emit('writing', username);
+        });
+
+        //escucho el evento socket emitido en el cliente
         socket.on('message', (data) => {
             messageslist.push(data);
             io_Client.emit('messageslist', messageslist);
