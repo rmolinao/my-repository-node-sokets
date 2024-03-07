4e5b402670cd94640bc963b1636b2797cdb738d0
Rafael Molina Osias <engineer.raflash@gmail.com>
2024-03-07 07:31:53 -0500

    emitiendo eventos con socket segunda parte simulacion basica de un chat

--- src/public/index.js
+++ src/public/index.js
@@ -1,19 +1,31 @@
 const socket = io();
 
 const username = document.getElementById('username');
-const white_mesage = document.getElementById('white_mesage');
+const white_message = document.getElementById('white_message');
+const all_messages = document.getElementById('all_messages');
 
-white_mesage.addEventListener('keyup',evnt => {
+white_message.addEventListener('keyup',evnt => {
     if (evnt.code == 'Enter') {
-        if (username.value != '' && white_mesage.value != '') {
+        if (username.value != '' && white_message.value != '') {
             //evento socket
             socket.emit('message',{
                 username: username.value,
-                mesage  : white_mesage.value.slice(0, -1)
+                message  : white_message.value.slice(0, -1)
             });
-            white_mesage.value ='';
+            white_message.value ='';
         }else{
             console.log('Ingrese los campos Completos');
         }
     }
+});
+socket.on('messageslist', (messageslist) => {
+    let content = '';
+    messageslist.forEach( messages => {
+        content +=
+        `<div class='message'>
+            ${messages.username} : ${messages.message}:
+        </div><br>`;
+    });
+    all_messages.innerHTML = content;
+    all_messages.scrollTop = all_messages.scrollHeight;
 });
--- src/sokect.js
+++ src/sokect.js
@@ -1,9 +1,12 @@
+let messageslist = [];
 module.exports = io_Client => {
     io_Client.on('connection', (socket) => {
         console.log('Un usuario se ha conectado');
+        io_Client.emit('messageslist', messageslist);
         //escucho el evento socket en emitido en el cliente
         socket.on('message', (data) => {
-            console.log(data)
+            messageslist.push(data);
+            io_Client.emit('messageslist', messageslist);
         });
 
         socket.on('disconnect', () => {
