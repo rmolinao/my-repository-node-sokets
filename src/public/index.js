8688986e014afa2f42f3bcf5c203b84850fbe2bd
Rafael Molina Osias <engineer.raflash@gmail.com>
2024-03-06 22:55:40 -0500

    emitiendo eventos con socket primera parte

--- src/public/index.js
+++ src/public/index.js
@@ -1,1 +1,19 @@
 const socket = io();
+
+const username = document.getElementById('username');
+const white_mesage = document.getElementById('white_mesage');
+
+white_mesage.addEventListener('keyup',evnt => {
+    if (evnt.code == 'Enter') {
+        if (username.value != '' && white_mesage.value != '') {
+            //evento socket
+            socket.emit('message',{
+                username: username.value,
+                mesage  : white_mesage.value.slice(0, -1)
+            });
+            white_mesage.value ='';
+        }else{
+            console.log('Ingrese los campos Completos');
+        }
+    }
+});
--- src/sokect.js
+++ src/sokect.js
@@ -1,9 +1,13 @@
 module.exports = io_Client => {
-    io_Client.on('connection', socket => {
+    io_Client.on('connection', (socket) => {
         console.log('Un usuario se ha conectado');
+        //escucho el evento socket en emitido en el cliente
+        socket.on('message', (data) => {
+            console.log(data)
+        });
+
         socket.on('disconnect', () => {
             console.log('El usuario se ha desconectado');
         });
-
     });
 }
